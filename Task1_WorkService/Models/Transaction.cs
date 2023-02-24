584aedb0125925a72838ed67777b14154244f460
sashaFPV <andrei.cazanov2017@gmail.com>
2023-02-24 12:41:23 +0100

    init commit & basic model transaction

---
+++ Task1_WorkService/Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_WorkService.Models {
+    internal class Transaction {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public decimal Payment { get; set; } = decimal.Zero;
+        public DateOnly Date { get; set; } = new DateOnly();
+        public long AccountNumber { get; set; } = 0;
+        public string Service { get; set; } = string.Empty;
+    }
+}
