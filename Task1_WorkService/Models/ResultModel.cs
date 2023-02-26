3d042e9c459a25ced473c7ede31a94a6e5a8322a
sashaFPV <andrei.cazanov2017@gmail.com>
2023-02-26 19:35:10 +0100

    parsing with regex works

---
+++ Task1_WorkService/Models/ResultModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Task1_WorkService.Models {
+    internal class ResultModel {
+        [JsonPropertyName("city")]
+        public string City { get; set; } = string.Empty;
+        [JsonPropertyName("services")]
+        public List<Service> Services { get; set; } = new List<Service>();
+        public decimal Total { get; set; } = decimal.Zero;
+    }
+    internal class Service {
+        [JsonPropertyName("name")]
+        public string ServiceName { get; set; } = string.Empty;
+        [JsonPropertyName("payers")]
+        public List<Payer> Payers { get; set; } = new List<Payer>();
+        public decimal Total { get; set; } = decimal.Zero;
+    }
+    internal class Payer {
+        [JsonPropertyName("name")]
+        public string PayerName { get; set; } = string.Empty;
+        [JsonPropertyName("payment")]
+        public decimal Payment { get; set; } = decimal.Zero;
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; } = DateTime.MinValue;
+        [JsonPropertyName("account_number")]
+        public long AccountNumber { get; set; }
+    }
+}
--- Task1_WorkService/MyFileReader.cs
+++ Task1_WorkService/MyFileReader.cs
@@ -9,37 +9,61 @@ using Task1_WorkService.Models;
 namespace Task1_WorkService {
     internal static class MyFileReader {
         private const int DefaultBufferSize = 8192; // or 4096
-        public static async Task<List<UserTransaction>> ReadAllLinesAsync(string path) {
+        public static async Task<List<UserTransaction>> ReadAllLinesAsync(FileInfo file) {
             var transactions = new List<UserTransaction>();
+            try {
+                if (!IsFileLocked(file)) {
+                    using (var stream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, DefaultBufferSize)) {
+                        using (var reader = new StreamReader(stream, Encoding.UTF8)) {
+                            string line;
+                            UserTransaction transaction = new UserTransaction();
+                            while ((line = await reader.ReadLineAsync()) != null) {
+                                if(RegexTransactionGenerator.IsNormalTransaction(line)) {
+                                    var groups = RegexTransactionGenerator.SplitTransaction(line);
+                                    if(groups.Length == 9) {
+                                        // Parsing data
+                                        transaction.FirstName = groups[0];
+                                        transaction.LastName = groups[1];
+                                        transaction.Address = groups[2].Trim('“', '”', '"', '`');
+                                        transaction.Service = groups[8];
 
-            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, DefaultBufferSize)) {
-                using (var reader = new StreamReader(stream, Encoding.UTF8)) {
-                    string line;
-                    UserTransaction transaction = new UserTransaction();
-                    while ((line = await reader.ReadLineAsync()) != null) {
-                        if(RegexTransactionGenerator.IsNormalTransaction(line)) {
-                            var groups = RegexTransactionGenerator.SplitTransaction(line);
-                            transaction.FirstName = groups[0];
-                            transaction.LastName = groups[1];
-                            transaction.Address = groups[2].Trim('“', '”', '"', '`');
-                            transaction.Service = groups[8];
-
-                            if (Decimal.TryParse(groups[5], out decimal payment)) {
-                                transaction.Payment = payment;
+                                        if (Decimal.TryParse(groups[5], out decimal payment)) {
+                                            transaction.Payment = payment;
+                                        }
+                                        if (DateOnly.TryParse(groups[6], out DateOnly date)) {
+                                            transaction.Date = date;
+                                        }
+                                        if (long.TryParse(groups[7], out long accountNumber)) {
+                                            transaction.AccountNumber = accountNumber;
+                                        }
+                                        transactions.Add(transaction);
+                                    }
+                                }
                             }
-                            if (DateOnly.TryParse(groups[6], out DateOnly date)) {
-                                transaction.Date = date;
-                            }
-                            if (long.TryParse(groups[7], out long accountNumber)) {
-                                transaction.AccountNumber = accountNumber;
-                            }
-                            transactions.Add(transaction);
                         }
                     }
                 }
             }
-
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
             return transactions;
+        }
+        public static bool IsFileLocked(FileInfo file) {
+            try {
+                using (FileStream stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None)) {
+                    stream.Close();
+                }
+            }
+            catch (IOException) {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            //file is not locked
+            return false;
         }
     }
 }
